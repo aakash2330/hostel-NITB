8cda3b42c404c50b6bdf6c49673548160c42eb69
Aakash Singh <78924853+aakash2330@users.noreply.github.com>
2024-05-19 21:09:28 +0530

    Update auth.ts

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -64,7 +64,7 @@ export const authOptions: NextAuthOptions = {
           id: profile.sub,
           name: `${profile.given_name}`,
           email: profile.email,
-          role: UserPermissionRole.USER,
+          role: UserPermissionRole.ADMIN,
         };
       },
     }),
