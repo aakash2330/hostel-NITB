c0dde192184a235540b83487d66067c8e99d3aa9
saurabhp0007 <saurabhpanday21212@gmail.com>
2024-06-13 11:22:51 +0530

    individual page fix ~Saurabh

--- src/app/_components/single-hostel.tsx
+++ src/app/_components/single-hostel.tsx
@@ -1,42 +1,67 @@
-'use client';
-import { useSession } from 'next-auth/react';
-import React from 'react';
+"use client";
+import { useSession } from "next-auth/react";
+import React from "react";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "~/components/ui/accordion"
-import { Fragment, useState } from 'react';
-import {
-  Tab,
-} from '@headlessui/react';
-import { classNames } from '~/lib/classNames';
-import Link from 'next/link';
-import { GuestHouse, RoomDetails } from '@prisma/client';
-import { TbookingType } from '~/lib/utils';
+} from "~/components/ui/accordion";
+import { Fragment, useState } from "react";
+import { Tab } from "@headlessui/react";
+import { classNames } from "~/lib/classNames";
+import Link from "next/link";
+import { GuestHouse, RoomDetails } from "@prisma/client";
+import { TbookingType } from "~/lib/utils";
+import { FaHotTub } from "react-icons/fa";
 
 const roomFeatures = {
   EXECUTIVE_GUEST_HOUSE: {
-    description: "Experience comfort and convenience in our thoughtfully appointed single room at Saran Guesthouse, nestled within the serene campus of NITTTR Bhopal. Perfect for solo travelers seeking a peaceful retreat, our single room offers a cozy sanctuary equipped with modern amenities for a relaxing stay. Immerse yourself in tranquility while enjoying easy access to nearby attractions and facilities. Book your stay with us for a rejuvenating experience in the heart of Bhopal.",
-    features: ["Double Bed: Sink into plush bedding for a restful night's sleep", "Ensuite Bathroom: Enjoy the convenience of a private bathroom equipped with modern amenities.", "Workspace: Stay productive with a dedicated workspace, ideal for business travelers.", "Complimentary Wi-Fi: Stay connected with high-speed internet access throughout your stay.", "Air Conditioning: Stay cool and comfortable in any season."]
+    description:
+      "Experience comfort and convenience in our thoughtfully appointed single room at Saran Guesthouse, nestled within the serene campus of NITTTR Bhopal. Perfect for solo travelers seeking a peaceful retreat, our single room offers a cozy sanctuary equipped with modern amenities for a relaxing stay. Immerse yourself in tranquility while enjoying easy access to nearby attractions and facilities. Book your stay with us for a rejuvenating experience in the heart of Bhopal.",
+    features: [
+      "Double Bed: Sink into plush bedding for a restful night's sleep",
+      "Ensuite Bathroom: Enjoy the convenience of a private bathroom equipped with modern amenities.",
+      "Workspace: Stay productive with a dedicated workspace, ideal for business travelers.",
+      "Complimentary Wi-Fi: Stay connected with high-speed internet access throughout your stay.",
+      "Air Conditioning: Stay cool and comfortable in any season.",
+    ],
   },
   SARAN_GUEST_HOUSE: {
-    description: "Discover spacious and cozy double rooms at NITTTR Bhopal, ideal for couples or friends traveling together. Enjoy a comfortable stay with modern amenities. Book your room now!",
-    features: ["attached toilets and bathrooms", "It has separate kitchen and dining facility", " All the rooms are air conditioned and equipped with all modern amenities."]
+    description:
+      "Discover spacious and cozy double rooms at NITTTR Bhopal, ideal for couples or friends traveling together. Enjoy a comfortable stay with modern amenities. Book your room now!",
+    features: [
+      "attached toilets and bathrooms",
+      "It has separate kitchen and dining facility",
+      " All the rooms are air conditioned and equipped with all modern amenities.",
+    ],
   },
   VISHVESHVARAYA_GUEST_HOUSE: {
-    description: "It has G +1 floor with 26 double bedded rooms with attached toilets and bathrooms. It has separate kitchen and dining facility. All the rooms are air conditioned and equipped with all modern amenities.",
-    features: ["attached toilets and bathrooms", "It has separate kitchen and dining facility", " All the rooms are air conditioned and equipped with all modern amenities."]
-  }
-}
+    description:
+      "It has G +1 floor with 26 double bedded rooms with attached toilets and bathrooms. It has separate kitchen and dining facility. All the rooms are air conditioned and equipped with all modern amenities.",
+    features: [
+      "attached toilets and bathrooms",
+      "It has separate kitchen and dining facility",
+      " All the rooms are air conditioned and equipped with all modern amenities.",
+    ],
+  },
+};
 
-function ProductHeroSlider({ roomDetails, checkin, checkout, bookingType }: { roomDetails: RoomDetails, checkin: Date, checkout: Date, bookingType: TbookingType }) {
+export default function ProductHeroSlider({
+  roomDetails,
+  checkin,
+  checkout,
+  bookingType,
+}: {
+  roomDetails: RoomDetails;
+  checkin: Date;
+  checkout: Date;
+  bookingType: TbookingType;
+}) {
   const { data: session } = useSession();
   const [open, setOpen] = useState(false);
 
-
-  console.log(roomDetails,"dasdas")
+  console.log(roomDetails, "dasdas");
   return (
     <div className="bg-white">
       <main className="mx-auto max-w-7xl sm:px-6 sm:py-16 lg:px-8">
@@ -46,8 +71,8 @@ function ProductHeroSlider({ roomDetails, checkin, checkout, bookingType }: { ro
             {/* Image gallery */}
             <Tab.Group as="div" className="flex flex-col-reverse">
               {/* Image selector */}
-              <div className="mx-auto p-2 mt-6 w-full max-w-2xl lg:max-w-none">
-                <Tab.List className="grid grid-cols-4 gap-6 ">
+              <div className="mx-auto mt-6 w-full max-w-2xl p-2 lg:max-w-none">
+                <Tab.List className="grid grid-cols-4 gap-6">
                   {roomDetails.roomImg.map((image, index) => (
                     <Tab
                       key={image + index}
@@ -65,15 +90,14 @@ function ProductHeroSlider({ roomDetails, checkin, checkout, bookingType }: { ro
                           </span>
                           <span
                             className={classNames(
-                              selected ? 'ring-indigo-500' : 'ring-transparent',
-                              'pointer-events-none absolute inset-0 rounded-md ring-2 ring-offset-2'
+                              selected ? "ring-indigo-500" : "ring-transparent",
+                              "pointer-events-none absolute inset-0 rounded-md ring-2 ring-offset-2",
                             )}
                             aria-hidden="true"
                           />
                         </>
                       )}
                     </Tab>
-
                   ))}
                 </Tab.List>
               </div>
@@ -93,89 +117,106 @@ function ProductHeroSlider({ roomDetails, checkin, checkout, bookingType }: { ro
 
             {/* Product info */}
             <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
-              <h1 className="text-lg md:text-3xl font-bold tracking-tight text-gray-900">
-                {roomDetails?.hostelName}
+              <h1 className="text-2xl font-bold tracking-tight text-gray-900 md:text-3xl">
+                {roomDetails?.hostelName.replace(/_/g, " ")}
               </h1>
-              <h2>{roomDetails?.floor}</h2>
 
-              <div className='flex flex-col gap-2'>
-                <span>features</span>
-                <span>geaser:{roomDetails?.geaser}</span>
-                <span>ac:{roomDetails?.ac}</span>
-                <span>max adult:{roomDetails?.maxAdult}</span>
-                <span>max child :{roomDetails?.maxChild}</span>
+              <div className="flex gap-4 pt-4">
+                <h2 className="rounded-xl bg-gray-200 p-2 text-lg font-semibold text-gray-700">
+                  {roomDetails?.floor?.replace(/_/g, " ")}
+                </h2>
+                <h2
+                  className={`rounded-xl p-2 text-lg font-semibold text-gray-100 ${roomDetails?.bookingStatus === "CANCELED" ? "bg-green-500" : "bg-red-400"}`}
+                >
+                  {roomDetails?.bookingStatus === "CANCELED"
+                    ? " Available"
+                    : "Not Available"}
+                </h2>
+
+                <h2 className="rounded-xl bg-gray-200 p-2 text-lg font-semibold text-gray-700">
+                  {roomDetails?.value}
+                </h2>
               </div>
-
-              <div className="mt-3">
-
-                <h2 className="sr-only">Product information</h2>
-                <p className="text-3xl tracking-tight text-gray-900">
-                  {roomDetails.totalChargePerDay}
-                </p>
-              </div>
-
-              {/* Reviews */}
-              <div className="mt-3">
-                <h3 className="sr-only">Reviews</h3>
-                <div className="flex items-center">
-                  <div className="flex items-center">
-                  </div>
-                  <p className="sr-only">{"product.rating"} out of 5 stars</p>
-                </div>
+              <div className="mt-4">
+                <h3 className="text-lg font-semibold text-gray-900">
+                  Features
+                </h3>
+                <ul className="mt-2 space-y-2">
+                  <li className="flex items-baseline gap-2">
+                    {" "}
+                    <FaHotTub /> Geaser: {roomDetails?.geaser}
+                  </li>
+                  <li className="flex items-baseline gap-2">
+                    {" "}
+                    <FaHotTub />
+                    AC: {roomDetails?.ac}
+                  </li>
+                  <li className="flex items-baseline gap-2">
+                    <FaHotTub /> Max Adult Allowed: {roomDetails?.maxAdult}
+                  </li>
+                  <li className="flex items-baseline gap-2">
+                    <FaHotTub /> Max Child Allowed: {roomDetails?.maxChild}
+                  </li>
+                  <li className="flex items-baseline gap-2">
+                    <FaHotTub /> Occupancy: {roomDetails?.occupancy}
+                  </li>
+                </ul>
               </div>
 
               <div className="mt-6">
-                <h3 className="sr-only">Description</h3>
-
-                <div
-                  className="space-y-6 text-base text-gray-700"
-                  dangerouslySetInnerHTML={{ __html: roomFeatures[roomDetails.hostelName].description }}
-                />
+                <h3 className="text-lg font-semibold text-gray-900">
+                  Product Information
+                </h3>
+                <p className="mt-2 text-base text-gray-700">
+                  {roomDetails.remark}
+                </p>
               </div>
 
               <form className="mt-6">
-                {/* Colors */}
-
                 <div className="mt-10 flex">
-                  <Link href={session ? `/checkout?id=${roomDetails.id}&checkin=${checkin}&checkout=${checkout}&type=${bookingType}` : "/login"} className='w-full'>
+                  <Link
+                    href={
+                      session
+                        ? `/checkout?id=${roomDetails.id}&checkin=${checkin}&checkout=${checkout}&type=${bookingType}`
+                        : "/login"
+                    }
+                    passHref
+                    className="w-full"
+                  >
                     <button
                       type="submit"
-                      className="flex max-w-xs flex-1 items-center justify-center rounded-md border border-transparent bg-primary px-8 py-3 text-base font-medium text-white hover:bg-primary focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50 sm:w-full"
+                      className="hover:bg-primary-dark flex max-w-xs flex-1 items-center justify-center rounded-md border border-transparent bg-primary px-8 py-3 text-base font-medium text-white focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-full"
                     >
                       Book Now
                     </button>
                   </Link>
-
-                  <button
-                    type="button"
-                    className="ml-4 flex items-center justify-center rounded-md px-3 py-3 text-gray-400 hover:bg-gray-100 hover:text-gray-500"
-                  >
-                    <span className="sr-only">Add to favorites</span>
-                  </button>
                 </div>
               </form>
 
               <section aria-labelledby="details-heading" className="mt-12">
-                <h2 id="details-heading" className="sr-only">
-                  Additional details
+                <h2
+                  id="details-heading"
+                  className="text-lg font-semibold text-gray-900"
+                >
+                  Additional Details
                 </h2>
-                <Accordion type="single" collapsible className="w-full">
-                  <AccordionItem value="item-1">
-                    <AccordionTrigger>Is it Available?</AccordionTrigger>
-                    <AccordionContent>Yes</AccordionContent>
-                  </AccordionItem>
+                <Accordion type="single" collapsible className="mt-4 w-full">
                   <AccordionItem value="item-2">
-                    <AccordionTrigger>Features</AccordionTrigger>
-                    {roomFeatures[roomDetails.hostelName].features.map((d, index) => {
-                      return <AccordionContent key={d + index}>
-                        {d}
-                      </AccordionContent>
-                    })}
-
+                    <AccordionTrigger className="text-base font-medium text-gray-700">
+                      Features
+                    </AccordionTrigger>
+                    {roomFeatures[roomDetails.hostelName].features.map(
+                      (d, index) => (
+                        <AccordionContent
+                          key={d + index}
+                          className="mt-2 text-sm text-gray-600"
+                        >
+                          {d}
+                        </AccordionContent>
+                      ),
+                    )}
                   </AccordionItem>
-
                 </Accordion>
-
               </section>
             </div>
           </div>
@@ -184,5 +225,3 @@ function ProductHeroSlider({ roomDetails, checkin, checkout, bookingType }: { ro
     </div>
   );
 }
-
-export default ProductHeroSlider;
